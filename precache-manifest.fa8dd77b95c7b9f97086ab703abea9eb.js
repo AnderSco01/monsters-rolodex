0a8bd2f5b89b7953db4866d4544299564d0b1cfc
Scott Anderson <scott.anderson@avamae.co.uk>
2019-08-27 12:05:52 +0100

    Updates

---
+++ precache-manifest.fa8dd77b95c7b9f97086ab703abea9eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d613ad31c848379bfd58fd8464809a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ba6d69eee7b835384ddc",
+    "url": "/monsters-rolodex/static/css/main.453793a6.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "ba6d69eee7b835384ddc",
+    "url": "/monsters-rolodex/static/js/main.880cd498.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
